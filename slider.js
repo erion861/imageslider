cb74fb3d5071414aea8639cf5782f0fc95e47fa9
Moe <67797901+erion861@users.noreply.github.com>
2021-02-12 20:30:18 +0000

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,19 +1,20 @@
 const prevBtn = document.getElementById('btn-left');
 const nextBtn = document.getElementById('btn-right');
 const container = document.querySelector('.slide');
-const images = Array.from(document.querySelectorAll('.slide img'));
+// const images = Array.from(document.querySelectorAll('.slide img'));
+const images = document.querySelector('.slide').children;
 
 console.log(prevBtn);
 console.log(nextBtn);
 console.log(container);
 console.log(images);
 
-let counter = 0;
+let i = 0;
 
 nextBtn.addEventListener('click', ()=>{
-  counter++;
-  if(counter >= 3) return;
-  console.log(counter);
+  for(i=0; i < images.length; i++) {
+    
+  };
 });
 
 prevBtn.addEventListener('click', ()=>{
