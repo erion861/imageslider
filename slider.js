759aeb16471b75291dce4ff0eda24a1138cbd432
Moe <67797901+erion861@users.noreply.github.com>
2021-02-13 17:13:50 +0000

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,24 +1,54 @@
 const prevBtn = document.getElementById('btn-left');
 const nextBtn = document.getElementById('btn-right');
 const container = document.querySelector('.slide');
-// const images = Array.from(document.querySelectorAll('.slide img'));
 const images = document.querySelector('.slide').children;
+
+let counter = 0;
 
 console.log(prevBtn);
 console.log(nextBtn);
 console.log(container);
 console.log(images);
-
-// let i = 0;
+console.log(images[0]);
+console.log(images[1]);
+console.log(images[2]);
 
 nextBtn.addEventListener('click', ()=>{
-  for(i = 0; i < images.length; i++) {
-    container.classList.add('show');
-  };
+  counter++;
+  console.log(counter);
+  if(counter === 1) {
+    images[0].classList.remove('show');
+    images[1].classList.add('show');
+  } 
+  if(counter === 2) {
+    images[0].classList.remove('show');
+    images[1].classList.remove('show');
+    images[2].classList.add('show');
+  }
+  if(counter >= 3) {
+    counter = 0;
+    images[0].classList.add('show');
+    images[1].classList.remove('show');
+    images[2].classList.remove('show');
+  }
 });
 
 prevBtn.addEventListener('click', ()=>{
-  for(i = 0; i < images.length; i++) {
-    container.classList.remove('show');
-  };
+  counter++;
+  console.log(counter);
+  if(counter === 1) {
+    images[0].classList.remove('show');
+    images[1].classList.add('show');
+  } 
+  if(counter === 2) {
+    images[0].classList.remove('show');
+    images[1].classList.remove('show');
+    images[2].classList.add('show');
+  }
+  if(counter >= 3) {
+    counter = 0;
+    images[0].classList.add('show');
+    images[1].classList.remove('show');
+    images[2].classList.remove('show');
+  }
 });
