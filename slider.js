a513171bb458cfaa84a627a73902b4e9fe45c016
Moe <67797901+erion861@users.noreply.github.com>
2021-02-14 17:50:56 +0000

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,54 +1,39 @@
 const prevBtn = document.getElementById('btn-left');
 const nextBtn = document.getElementById('btn-right');
-const container = document.querySelector('.slide');
 const images = document.querySelector('.slide').children;
 
 let counter = 0;
 
-console.log(prevBtn);
-console.log(nextBtn);
-console.log(container);
-console.log(images);
-console.log(images[0]);
-console.log(images[1]);
-console.log(images[2]);
-
 nextBtn.addEventListener('click', ()=>{
   counter++;
-  console.log(counter);
   if(counter === 1) {
-    images[0].classList.remove('show');
-    images[1].classList.add('show');
+    images[counter].classList.add('show');
+    images[counter - 1].classList.remove('show');
   } 
   if(counter === 2) {
-    images[0].classList.remove('show');
-    images[1].classList.remove('show');
-    images[2].classList.add('show');
+    images[counter].classList.add('show');
+    images[counter - 1].classList.remove('show');
   }
   if(counter >= 3) {
     counter = 0;
+    images[1,2].classList.remove('show');
     images[0].classList.add('show');
-    images[1].classList.remove('show');
-    images[2].classList.remove('show');
   }
 });
 
 prevBtn.addEventListener('click', ()=>{
   counter++;
-  console.log(counter);
   if(counter === 1) {
-    images[0].classList.remove('show');
-    images[1].classList.add('show');
+    images[counter].classList.add('show');
+    images[counter - 1].classList.remove('show');
   } 
   if(counter === 2) {
-    images[0].classList.remove('show');
-    images[1].classList.remove('show');
-    images[2].classList.add('show');
+    images[counter].classList.add('show');
+    images[counter - 1].classList.remove('show');
   }
   if(counter >= 3) {
     counter = 0;
+    images[1,2].classList.remove('show');
     images[0].classList.add('show');
-    images[1].classList.remove('show');
-    images[2].classList.remove('show');
   }
 });
